66738cc89a3ad6842978ae766cd91f0fcb04d8cd
ademarfsj <ademarfsj@gmail.com>
2016-04-13 09:17:50 -0300

    q

--- src/main/java/br/ufg/inf/fs/slum/rest/StatusService.java
+++ src/main/java/br/ufg/inf/fs/slum/rest/StatusService.java
@@ -9,7 +9,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response;
 
 /**
  * @author Ademar
